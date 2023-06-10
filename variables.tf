77220419e4409d23a571a1eb0a1c2b3c091f0727
kaizenacademy <kaizenacademy@gmail.com>
2023-06-10 08:51:52 +0000

    new changes

--- variables.tf
+++ variables.tf
@@ -1,48 +1,48 @@
-# variable ami_id {
-#   type        = string
-#   default     = ""
-#   description = "Proved AMI id"
-# }
-
-# variable instance_type {
-#   type        = string
-#   default     = ""
-#   description = "Proved Instance type"
-# }
-
-# variable az1 {
-#   type        = string
-#   default     = ""
-#   description = "Proved availability zone"
-# }
-
-# variable key_pair {
-#   type        = string
-#   default     = ""
-#   description = "Proved key pair name"
-# }
-
-
 variable ami_id {
   type        = string
-  default     = "ami-092b51d9008adea15"
+  default     = ""
   description = "Proved AMI id"
 }
 
 variable instance_type {
   type        = string
-  default     = "t2.micro"
+  default     = ""
   description = "Proved Instance type"
 }
 
 variable az1 {
   type        = string
-  default     = "us-east-2a"
+  default     = ""
   description = "Proved availability zone"
 }
 
 variable key_pair {
   type        = string
-  default     = "my-laptop-key"
+  default     = ""
   description = "Proved key pair name"
 }
+
+
+# variable ami_id {
+#   type        = string
+#   default     = "ami-092b51d9008adea15"
+#   description = "Proved AMI id"
+# }
+
+# variable instance_type {
+#   type        = string
+#   default     = "t2.micro"
+#   description = "Proved Instance type"
+# }
+
+# variable az1 {
+#   type        = string
+#   default     = "us-east-2a"
+#   description = "Proved availability zone"
+# }
+
+# variable key_pair {
+#   type        = string
+#   default     = "my-laptop-key"
+#   description = "Proved key pair name"
+# }
