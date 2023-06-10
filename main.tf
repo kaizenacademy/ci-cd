65cb0dc11270f841ba47851118d174888713a64f
kaizenacademy <kaizenacademy@gmail.com>
2023-06-10 08:08:09 +0000

    some changes

--- main.tf
+++ main.tf
@@ -1,10 +1,23 @@
+# provider "aws" {
+#   region = "us-east-2"
+# }
+
+# resource "aws_instance" "web" {
+#   ami                    = var.ami_id
+#   instance_type          = var.instance_type
+#   availability_zone      = var.az1
+#   key_name               = var.key_pair
+# }
+
+
 provider "aws" {
-  region = "us-east-2"
+region = "us-east-2"
 }
 
+
 resource "aws_instance" "web" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  availability_zone      = var.az1
-  key_name               = var.key_pair
+ami = var.ami_id
+instance_type = var.instance_type
+availability_zone = var.az1
+key_name = var.key_pair
 }
