6e0c7de9083f1d53133063b719e43391dcbaee5f
kaizenacademy <kaizenacademy@gmail.com>
2023-06-10 03:26:56 +0000

    modified variables

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable ami_id {
   type        = string
-  default     = "ami-092b51d9008adea15"
+  default     = ""
   description = "Proved AMI id"
 }
 
 variable instance_type {
   type        = string
-  default     = "t2.micro"
+  default     = ""
   description = "Proved Instance type"
 }
 
 variable az1 {
   type        = string
-  default     = "us-east-2a"
+  default     = ""
   description = "Proved availability zone"
 }
 
 variable key_pair {
   type        = string
-  default     = "my-laptop-key"
+  default     = ""
   description = "Proved key pair name"
 }
