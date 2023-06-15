af6210a4c59ac20b84fd89a0d1b30749660cbc2e
kaizenacademy <126742188+kaizenacademy@users.noreply.github.com>
2023-06-15 00:26:48 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = var.ami_id
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   availability_zone      = var.az1
   key_name               = var.key_pair
